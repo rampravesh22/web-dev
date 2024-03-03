5a522ba8eeefd93f11ce6c180eff779c118023e9
rpc1752 <rpc1752@gmail.com>
2024-03-03 11:31:09 +0530

    updated

--- fetch api/script.js
+++ fetch api/script.js
@@ -1,31 +1,31 @@
-const URL = "https://jsonplaceholder.typicode.com/posts"
+const URL = "https://jsonplaceholder.typicode.com/posts";
 
 // GET  and post request
 fetch(URL, {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'foo',
-            body: 'bar',
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw new Error("Somthing went wrong")
-        }
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log('catch');
-        console.log(error);
-    })
+	method: "POST",
+	body: JSON.stringify({
+		title: "foo",
+		body: "bar",
+		userId: 1,
+	}),
+	headers: {
+		"Content-type": "application/json; charset=UTF-8",
+	},
+})
+	.then((response) => {
+		if (response.ok) {
+			return response.json();
+		} else {
+			throw new Error("Somthing went wrong");
+		}
+	})
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((error) => {
+		console.log("catch");
+		console.log(error);
+	});
 
 // GET request
 // fetch(URL)
