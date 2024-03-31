20e1d7c9397e427eaaed7c947f4179b030a6707d
rpc1752 <rpc1752@gmail.com>
2024-03-31 09:28:53 +0530

    updated

--- test/script.js
+++ test/script.js
@@ -1,25 +1,7 @@
-const addBtn = document.querySelector(".add-btn");
-const input = document.querySelector("input");
-const listUl = document.querySelector(".list-ul");
-addBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	const list = document.createElement("li");
-	list.classList.add("list");
-	const editBtn = document.createElement("button");
-	const deleteBtn = document.createElement("button");
-	editBtn.textContent = "Edit";
-	deleteBtn.textContent = "Delete";
-	editBtn.classList.add("edit-btn");
-	deleteBtn.classList.add("delete-btn");
-	const listSpan = document.createElement("span");
-
-	listSpan.append(input.value);
-	list.append(listSpan, editBtn, deleteBtn);
-
-	list.append = input.value;
-	listUl.append(list);
-	input.value = "";
-});
-// MONGODB_URL = "mongodb+srv://sandeepkg8756:S1a2n3d4@cluster0.vphkibd.mongodb.net/"
-// PORT = 5000
-// SECRETE_KEY = 56ujyfhgfgasfea43fdfdgfdbxsacxbvjdrtzscv
+const input = prompt("Enter your name");
+if (input.length > 0) {
+	const container = document.createElement("div");
+	container.append(input);
+	container.classList.add("container");
+	document.body.append(container);
+}
